ef36d005378eb285f9e5d2db1f98260989feb302
Genival Rodrigues <professorgenival@hotmail.com>
2025-10-16 13:35:21 -0300

    deploy: versão staging pronta para Render

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,23 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
+// Configuração para local e Render
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
+  build: {
+    outDir: "dist", // pasta usada pelo Render
+  },
+  server: mode === 'development' ? {
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-      }
-    }
-  }
-})
+      },
+    },
+  } : undefined, // proxy só em dev, ignora no build
+}))
