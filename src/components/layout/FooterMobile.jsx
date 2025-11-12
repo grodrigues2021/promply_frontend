b90b0bf867de68aaeb5dd2eea94212b89fe24330
Genival Rodrigues <professorgenival@hotmail.com>
2025-11-12 08:19:11 -0300

    update: 12-11-2025 08:19:11

--- src/components/layout/FooterMobile.jsx
+++ src/components/layout/FooterMobile.jsx
@@ -1,7 +1,7 @@
 // src/components/layout/FooterMobile.jsx
 export default function FooterMobile({ user, handleLogout }) {
   return (
-    <div className="lg:hidden fixed bottom-0 left-0 w-full bg-gradient-to-r from-purple-600 to-blue-600 text-white px-4 py-3 flex items-center justify-between shadow-inner pb-[env(safe-area-inset-bottom)] z-50">
+<div className="lg:hidden w-full bg-gradient-to-r from-purple-600 to-blue-600 text-white px-4 py-3 flex items-center justify-between shadow-inner rounded-t-xl">
       <div className="flex flex-col">
         <span className="text-sm font-medium">
           Olá, {user?.name || "Usuário"}
